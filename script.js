666917bccd1fddcf2d4463b9d7ad65774cb8d9f3
basurajp <basurajp135@gmail.com>
2024-09-14 09:04:39 +0530

    intial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+// This script could later be used to dynamically add links or track clicks
+// document.querySelectorAll('.btn').forEach(button => {
+//     button.addEventListener('click', function() {
+//       alert('You clicked ' + this.innerText);
+//     });
+//   });
+  
